ecc622d74838c5d587268a349bdb0a94f1b2829e
Matías Sebastián Quezada Radovcic <145939113+Matias-QR@users.noreply.github.com>
2025-06-12 22:03:06 -0400

    fix: press-section.tsx, captions y enlaces actualizados a sitios. Revisar los que no funcionan correctamente

--- components/press-section.tsx
+++ components/press-section.tsx
@@ -16,127 +16,143 @@ export function PressSection() {
       src: "/imgs/prensa1.png",
       alt: "Fanny Torres en entrevista de televisión nacional",
       caption: "Entrevista en Radio Agricultura",
-      date: "Fecha",
+      date: "2024",
       url: "https://www.radioagricultura.cl/noticias/2023/10/01/fanny-torres-da-silva-entrevista/"
     },
     {
       id: 2,
       src: "/imgs/prensa2.png", 
-      alt: "Conferencia magistral en Universidad de Chile",
-      caption: "Conferencia magistral en Universidad de Chile",
-      date: "Fecha"
+      alt: "La Hora",
+      caption: "La Hora",
+      date: "2024",
+      url: "https://lahora.cl/estilo-de-vida/2024/07/19/autoconocimiento-y-relaciones-humanas-el-eneagrama-como-herramienta-para-conocernos-y-comprender-nuestro-rol-en-la-sociedad/"
     },
     {
       id: 3,
       src: "/imgs/prensa3.png",
-      alt: "Artículo en revista Paula",
-      caption: "Artículo destacado en Revista Paula",
-      date: "Fecha"
+      alt: "Artículo en La Cuarta",
+      caption: "Artículo destacado en La Cuarta",
+      date: "2024"
     },
     {
       id: 4,
       src: "/imgs/prensa4.png",
       alt: "Participación en programa radial",
       caption: "Entrevista en Radio Cooperativa",
-      date: "Fecha"
+      date: "2024",
+      url: "https://www.youtube.com/live/iT_lUkJXAmI"
     },
     {
       id: 5,
       src: "/imgs/prensa5.png",
-      alt: "Workshop empresarial",
-      caption: "Workshop para ejecutivos en Hotel W",
-      date: "Fecha"
+      alt: "Nación Web",
+      caption: "Artículo en Nación Web",
+      date: "2024",
+      url: "https://nacionweb.com.ar/la-autora-chilena-fanny-torres-silva-profundiza-en-el-autodescubrimiento-con-su-nuevo-libro-reconfigurando-el-ser-el-eneagrama-como-mapa-en-tiempos-de-crisis-2/"
     },
     {
       id: 6,
       src: "/imgs/prensa6.png",
-      alt: "Lanzamiento de libro",
-      caption: "Lanzamiento oficial del libro en Librería Antártica",
-      date: "Fecha"
+      alt: "Corrientes de Tarde",
+      caption: "Artículo en Corrientes de Tarde",
+      date: "2024",
+      url: "https://corrientesdetarde.com/index.php/bienestar-y-salud/revelando-los-nueve-arquetipos-del-eneagrama-reconfigurar-el-ser-de-fanny-torres-silva-proporciona-una-hoja-de-ruta-para-la-autocomprension-y-la-transformacion/"
     },
     {
       id: 7,
       src: "/imgs/prensa7.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Entrevista en Conexión Abierta",
+      caption: "Entrevista en Conexión Abierta",
+      date: "2024",
+      url: "https://www.youtube.com/watch?v=FMVyBESKl-Q"
     },
     {
       id: 8,
       src: "/imgs/prensa8.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Entrevista Alunízate",
+      caption: "Entrevista en Alunízate",
+      date: "2024",
+      url: "https://www.youtube.com/watch?v=WEY7ELHnsrc"
     },
     {
       id: 9,
       src: "/imgs/prensa9.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Artículo en DataBA",
+      caption: "Artículo en DataBA",
+      date: "2024",
+      url: "https://databa.ar/nota/5252/la-maestra-de-eneagrama-fanny-torres-silva-presenta-su-libro-reconfigurando-el-ser/"
     },
     {
       id: 10,
       src: "/imgs/prensa10.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Artículo en Dequeruza",
+      caption: "Artículo en dequeruza.ar",
+      date: "2024",
+      url: "https://dequeruza.ar/la-maestra-de-eneagrama-fanny-torres-silva-presenta-su-libro-reconfigurando-el-ser-el-eneagrama-como-mapa-en-tiempos-de-crisis-en-la-feria-internacional-del-libro-de-buenos-aires/"
     },
     {
       id: 11,
       src: "/imgs/prensa11.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Artículo en norteenlinea.com",
+      caption: "Artículo en norteenlinea.com",
+      date: "2024",
+      url: "https://norteenlinea.com/buenos-aires/la-maestra-de-eneagrama-fanny-torres-silva-presenta-su-libro-reconfigurando-el-ser-el-eneagrama-como-mapa-en-tiempos-de-crisis-en-la-feria-internacional-del-libro-de-buenos-aires"
     },
     {
       id: 12,
       src: "/imgs/prensa12.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Latamnoticias",
+      caption: "Artículo en Latam Noticias",
+      date: "2024",
+      url: "https://latamnoticias.com/informacion-general/fanny-torres-silva-presento-en-buenos-aires-su-libro-reconfigurando-el-ser-el-eneagrama-como-mapa-en-tiempos-de-crisis/"
     },
     {
       id: 13,
       src: "/imgs/prensa13.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Artículo en Buenos Aires no duerme",
+      caption: "Artículo en Buenos Aires No Duerme",
+      date: "2024",
+      url: "https://buenosairesnoduerme.com.ar/2024/05/04/fanny-torres-silva-la-maestra-de-eneagrama-presento-su-obra-en-la-feria-del-libro/"
     },
     {
       id: 14,
       src: "/imgs/prensa14.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Artículo en el Portal Bonaerense",
+      caption: "Artículo en el Portal Bonaerense",
+      date: "2024",
+      url: "https://elportalbonaerense.com.ar/la-maestra-de-eneagrama-fanny-torres-silva-presenta-su-libro-reconfigurando-el-ser-el-eneagrama-como-mapa-en-tiempos-de-crisis-en-la-feria-internacional-del-libro-de-buenos-aires/"
     },
     {
       id: 15,
       src: "/imgs/prensa15.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Artículo en informa BA",
+      caption: "Artículo en informa BA",
+      date: "2024",
+      url: "https://informaba.ar/la-maestra-de-eneagrama-fanny-torres-silva-presenta-su-libro-reconfigurando-el-ser-el-eneagrama-como-mapa-en-tiempos-de-crisis-en-la-feria-internacional-del-libro-de-buenos-aires/"
     },
     {
       id: 16,
       src: "/imgs/prensa16.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Artículo en Panorama Directo",
+      caption: "Artículo en Panorama Directo",
+      date: "2024",
+      url: "https://panoramadirecto.com/2024/05/fanny-torres-silva-presenta-reconfigurando-el-ser-el-eneagrama-como-mapa-en-tiempos-de-crisis-en-la-feria-del-libro.html"
     },
     {
       id: 17,
       src: "/imgs/prensa17.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Artículo en La Capital del Sol",
+      caption: "Artículo en La Capital del Sol USA",
+      date: "2024",
+      url: "https://lacapitaldelsol.com/fanny-torres-silva-presento-su-obra-reconfigurando-el-ser-el-eneagrama-como-mapa-en-tiempos-de-crisis-en-argentina/"
     },
     {
       id: 18,
       src: "/imgs/prensa18.png",
-      alt: "Lanzamiento de libro",
-      caption: "",
-      date: "Fecha"
+      alt: "Artículo en Telenews América",
+      caption: "Artículo en Telenews América",
+      date: "2024",
+      url: "https://www.telenewsamerica.com/2024/05/fanny-torres-silva-la-maestra-de.html"
     }
   ]
 
