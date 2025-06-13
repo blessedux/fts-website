bb89d9a948ebc459e4e38e53df6d90696401a197
Matías Sebastián Quezada Radovcic <145939113+Matias-QR@users.noreply.github.com>
2025-06-12 20:12:02 -0400

    fix en Footer.tsx cambiando el color a bg-gray-50 para el modo claro y dark:bg-gray-900 para el modo oscuro ya que el parallax se superponía

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,11 @@ import { Linkedin, Instagram, MessageCircle } from "lucide-react"
 
 export function Footer() {
   return (
+<<<<<<< HEAD
     <footer className="border-t py-8 bg-white">
+=======
+    <footer className="border-t py-8 bg-gray-50 dark:bg-gray-900">
+>>>>>>> 7caaec4 (fix en Footer.tsx cambiando el color a bg-gray-50 para el modo claro y dark:bg-gray-900 para el modo oscuro ya que el parallax se superponía)
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
           <div className="text-sm text-muted-foreground">
